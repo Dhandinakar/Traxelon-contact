d9c1cbd1c0bfbd04c9956acda047878112096a34
Dhanya Shetty <151926824+Dhandinakar@users.noreply.github.com>
2026-03-09 12:56:38 +0530

    first

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,7 @@ app.use(cors({
         "http://localhost:3000",
         "http://localhost:5173",
         "https://traxelon-main.vercel.app",
-        "https://traxeloncontact.vercel.app/"
-        // Add your Vercel URL here
+        "https://traxelon123.vercel.app",
     ],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
